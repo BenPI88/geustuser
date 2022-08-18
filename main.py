820815e42225065ff2cf63f136dfc0e1eb177336
BenPI88 <97198967+BenPI88@users.noreply.github.com>
2022-08-18 16:41:08 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import os
 while True:
-  os.system("gsettings set org.gnome.desktop.background picture-uri file:////home/guest/geustuser/bg1.jpg")
+  os.system("gsettings set org.gnome.desktop.background picture-uri file:////~/geustuser/bg1.jpg")
   os.system("gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'")
   os.system("gsettings set org.gnome.desktop.interface gtk-theme 'Yaru-red'")
   os.system("gsettings set org.gnome.desktop.background picture-uri file:////home/guest/geustuser/bg1.jpg")
