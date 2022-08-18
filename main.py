7487a17ab5906143ff9b22803f151249f5d60ae6
BenPI88 <97198967+BenPI88@users.noreply.github.com>
2022-08-18 16:25:18 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
-os.system("gsettings set org.gnome.desktop.background picture-uri file:////home/guest/pishare/bg.jpg")
-os.system("gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'")
-os.system("gsettings set org.gnome.desktop.interface gtk-theme 'Yaru-red'")
-os.system("gsettings set org.gnome.desktop.background picture-uri file:////home/guest/pishare/bg.jpg")
+while True:
+  os.system("gsettings set org.gnome.desktop.background picture-uri file:////home/guest/pishare/bg.jpg")
+  os.system("gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'")
+  os.system("gsettings set org.gnome.desktop.interface gtk-theme 'Yaru-red'")
+  os.system("gsettings set org.gnome.desktop.background picture-uri file:////home/guest/pishare/bg.jpg")
